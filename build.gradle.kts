b4f106a4ccb2a525b8ae728ce7d921d57e3ab664
Lawry Sorenson <lawrysorenson@gmail.com>
2021-10-05 18:40:37 -0600

    Generate new ktor server

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    application
+}
+
+group = "com.byu.cs452"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlinx") }
+    maven { url = uri("https://dl.bintray.com/kotlin/ktor") }
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+    implementation("io.ktor:ktor-server-netty:1.4.0")
+    implementation("io.ktor:ktor-html-builder:1.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "ServerKt"
+}
