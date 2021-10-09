aeff0cd46983d3e8570c9cffd0335f1089afb86f
JJ Thorley <thorjj98@byu.edi>
2021-10-09 13:19:08 -0600

    hackathon end

--- src/main/kotlin/Services/LanguageService.kt
+++ src/main/kotlin/Services/LanguageService.kt
@@ -5,7 +5,7 @@ import Responses.LanguageResponse
 class LanguageService {
     fun getLanguages(): LanguageResponse {
         //Implement DAO
-        val response = LanguageResponse(true, arrayOf("English", "Spanish"))
+        val response = LanguageResponse(true, arrayOf("English", "Spanish"), arrayOf("eng", "spa"))
         return response
     }
 }
