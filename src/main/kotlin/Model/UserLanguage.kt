6d4769c13fd7b2c4758e521178db0a007a909137
Irvin Mendoza <irvinmdoza@gmail.com>
2021-10-09 10:57:19 -0600

    Removes RelationID property from Userlanguagt

--- src/main/kotlin/Model/UserLanguage.kt
+++ src/main/kotlin/Model/UserLanguage.kt
@@ -1,8 +1,7 @@
-class UserLanguage (var relationshipID: String, var relationType: Int, var userID: String, var languageID: String) {
+class UserLanguage (var relationType: Int, var userID: String, var languageID: String) {
 
     override fun equals(other: Any?)
         = (other is UserLanguage)
-            && relationshipID == other.relationshipID
             && relationType == other.relationType
             && userID == other.userID
             && languageID == other.languageID
