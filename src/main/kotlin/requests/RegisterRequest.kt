6865a21f8a81a37216286ee91a0fdc0a9bac82f4
Lawry Sorenson <lawrysorenson@gmail.com>
2021-10-16 11:44:57 -0600

    Add register function

--- src/main/kotlin/requests/RegisterRequest.kt
+++ src/main/kotlin/requests/RegisterRequest.kt
@@ -2,4 +2,4 @@ package requests
 
 class RegisterRequest(var username: String, var password: String,
                       var firstName: String?, var lastName: String?,
-                      var nativeLanguage: String, var learningLanguage: String)
+                      var nativeLanguage: String)
--- src/main/kotlin/services/RegisterService.kt
+++ src/main/kotlin/services/RegisterService.kt
@@ -1,12 +1,39 @@
 package services
 
+import dao.AuthTokenDAO
+import dao.Database
+import dao.UserDAO
+import model.AuthToken
+import model.User
 import requests.RegisterRequest
+import responses.LoginResponse
 import responses.RegisterResponse
 
 class RegisterService {
-    fun register(request: RegisterRequest): RegisterResponse {
-        //Implement DAO
-        val response = RegisterResponse(true, null)
-        return response
+    fun register(req: RegisterRequest): RegisterResponse {
+        val db = Database()
+
+        return try {
+            val conn = db.openConnection()
+
+            //TODO: hash password in req
+            val user = User(0, req.username, req.password, req.firstName, req.lastName)
+
+            val udao = UserDAO(conn)
+            udao.insert(user)
+
+            //Successful insert, create auth token
+            val token = AuthToken(user.id)
+
+            val tdao = AuthTokenDAO(conn)
+            tdao.insert(token)
+
+            db.closeConnection(true)
+            RegisterResponse(true, token)
+
+        } catch (e: Exception) {
+            db.closeConnection(false)
+            RegisterResponse(false, null)
+        }
     }
 }
